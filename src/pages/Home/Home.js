b0f84b71b3bb452c3e061c974bb3525b879e6bfa
Daniel Cho <danielcho91@gmail.com>
2021-02-23 19:30:06 -0500

    global keyframe animations

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,38 +9,38 @@ const Home = () => {
       <div className={styles.content}>
         <div className={styles.greeting}>
           <span className={styles.greetingChild}>
-            <span>H</span>
-            <span className={styles.textAnimated}>i</span>
-            <span className={styles.textAnimated}>,</span>
+            <span className='Text-animated'>H</span>
+            <span className='Text-animated'>i</span>
+            <span className='Text-animated'>,</span>
           </span>
           <span className={styles.greetingChild}>
-            <span className={styles.textAnimated}>I</span>
-            <span className={styles.textAnimated}>'</span>
-            <span className={styles.textAnimated}>m</span>
-            <span className={styles.textAnimated}>&nbsp;</span>
-            <span className={styles.textAnimated}>D</span>
-            <span className={styles.textAnimated}>a</span>
-            <span className={styles.textAnimated}>n</span>
-            <span className={styles.textAnimated}>i</span>
-            <span className={styles.textAnimated}>e</span>
-            <span className={styles.textAnimated}>l</span>
-            <span className={styles.textAnimated}>,</span>
+            <span className='Text-animated'>I</span>
+            <span className='Text-animated'>'</span>
+            <span className='Text-animated'>m</span>
+            <span className='Text-animated'>&nbsp;</span>
+            <span className='Text-animated'>D</span>
+            <span className='Text-animated'>a</span>
+            <span className='Text-animated'>n</span>
+            <span className='Text-animated'>i</span>
+            <span className='Text-animated'>e</span>
+            <span className='Text-animated'>l</span>
+            <span className='Text-animated'>,</span>
           </span>
           <span className={styles.greetingChild}>
-            <span className={styles.textAnimated}>w</span>
-            <span className={styles.textAnimated}>e</span>
-            <span className={styles.textAnimated}>b</span>
-            <span className={styles.textAnimated}>&nbsp;</span>
-            <span className={styles.textAnimated}>d</span>
-            <span className={styles.textAnimated}>e</span>
-            <span className={styles.textAnimated}>v</span>
-            <span className={styles.textAnimated}>e</span>
-            <span className={styles.textAnimated}>l</span>
-            <span className={styles.textAnimated}>o</span>
-            <span className={styles.textAnimated}>p</span>
-            <span className={styles.textAnimated}>e</span>
-            <span className={styles.textAnimated}>r</span>
-            <span className={styles.textAnimated}>.</span>
+            <span className='Text-animated'>w</span>
+            <span className='Text-animated'>e</span>
+            <span className='Text-animated'>b</span>
+            <span className='Text-animated'>&nbsp;</span>
+            <span className='Text-animated'>d</span>
+            <span className='Text-animated'>e</span>
+            <span className='Text-animated'>v</span>
+            <span className='Text-animated'>e</span>
+            <span className='Text-animated'>l</span>
+            <span className='Text-animated'>o</span>
+            <span className='Text-animated'>p</span>
+            <span className='Text-animated'>e</span>
+            <span className='Text-animated'>r</span>
+            <span className='Text-animated'>.</span>
           </span>
         </div>
         <br />
